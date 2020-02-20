ebdf844e72fdfcebc2be39a3a4217f1693a14749
Lars Ingebrigtsen <larsi@gnus.org>
2020-02-20 17:35:53 +0100

    Start implementing books as objects

--- src/lantern/core.cljs
+++ src/lantern/core.cljs
@@ -5,8 +5,71 @@
 ;; -------------------------
 ;; Views
 
+(defn px [number]
+  (str number "px"))
+
+(defn img [image]
+  (str "url(https://quimby.gnus.org/circus/lanterne/" image ")"))
+
+(defn make-book [[book spine-width pages]]
+  (let [height 564
+        width 348
+        spine-width (/ spine-width 4)]
+    (prn book)
+    [:div.book
+     [:div.face.front
+      {:style {:background-image (img (str book "/01.jpg"))
+               :width (px width)
+               :height (px height)
+               :background-size (str (px width) " " (px height))
+               :transform (str "rotateY(0deg) translateZ("
+                               (px (/ spine-width 2)) ")")}}]
+     [:div.face.back
+      {:style {:background-image (img (str book "/02.jpg"))
+               :width (px width)
+               :height (px height)
+               :background-size (str (px width) " " (px height))
+               :transform (str "rotateY(180deg) translateZ("
+                               (px (/ spine-width 2)) ")")}}]
+     [:div.face.left
+      {:style {:background-image (img (str book "/03.jpg"))
+               :width (px spine-width)
+               :height (px height)
+               :background-size (str (px spine-width) " " (px height))
+               :left (px (- (/ width 2) (/ spine-width 2)))
+               :transform (str "rotateY(-90deg) translateZ("
+                               (px (/ width 2)) ")")}}]
+     [:div.face.right
+      {:style {:background-image (img "pages/01.jpg")
+               :width (px spine-width)
+               :height (px height)
+               :background-size (str (px spine-width) " " (px height))
+               :left (px (- (/ width 2) (/ spine-width 2)))
+               :transform (str "rotateY(90deg) translateZ("
+                               (px (/ width 2)) ")")}}]
+     [:div.face.top
+      {:style {:background-image (img "pages/02-rot.jpg")
+               :width (px width)
+               :height (px spine-width)
+               :background-size (str (px width) " " (px spine-width))
+               :top (px (- (/ height 2) (/ spine-width 2)))
+               :transform (str "rotateX(90deg) translateZ("
+                               (px (/ height 2)) ")")}}]
+     [:div.face.bottom
+      {:style {:background-image (img "pages/03-rot.jpg")
+               :width (px width)
+               :height (px spine-width)
+               :background-size (str (px width) " " (px spine-width))
+               :top (px (- (/ height 2) (/ spine-width 2)))
+               :transform (str "rotateX(-90deg) translateZ("
+                               (px (/ height 2)) ")")}}]]
+    ))
+
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+  (let [bs (js->clj js/books)]
+    [:div
+     [:h2 "Lantern"]
+     (make-book (nth bs 1))]))
 
 ;; -------------------------
 ;; Initialize app
